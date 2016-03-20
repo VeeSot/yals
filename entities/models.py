2f12d05e2d73c7e60923acefc5c40817fe7c7ad3
Vee Sot <veesot@devdrive.ru>
2016-03-20 18:46:07 +0500

    Changing service add learn data

--- entities/models.py
+++ entities/models.py
@@ -1,7 +1,10 @@
 from typing import List
 from main import db
+import re
 
 __author__ = 'veesot'
+re_sequence = re.compile('([\d]+)\.')
+re_channel = re.compile('(\d+$)')
 
 
 class Entity(db.DynamicDocument):
@@ -10,21 +13,25 @@ class Entity(db.DynamicDocument):
     sequence = db.StringField(max_length=7, required=True)
     # Channel for signal.Range from 0 to 9.
     __channel = db.IntField(max_length=1, required=True, default=-1)
-    # Set of frequency for possibly channels.For example - [0,0,1,2,0,0,5,0,0]
-    __frequencies = db.ListField(db.IntField(), default=[0, 0, 0, 0, 0, 0, 0, 0, 0])
+    # Set of frequency for possibly channels.For example - [0,0,1,2,0,0,5,0,0,1]
+    __frequencies = db.ListField(db.IntField(), default=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
     def get_channel(self) -> int:
         """Returns channel number"""
         return self.__channel
 
+    def get_frequencies(self) -> List[int]:
+        """Returns list of freq"""
+        return self.__frequencies
+
     def _set_channel(self, new_chanel: int):
         self.__channel = new_chanel
 
-    def _set_frequencies(self, new_frequencies: List[int]):
+    def set_frequencies(self, new_frequencies: List[int]):
         self.__frequencies = new_frequencies
 
     def reset_frequencies(self):
-        self._set_frequencies([0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.set_frequencies([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
     def reset_channels(self):
         self._set_channel(-1)
@@ -34,6 +41,7 @@ class Entity(db.DynamicDocument):
         max_value = max(self.__frequencies)
         channel_number = self.__frequencies.index(max_value)
         self._set_channel(channel_number)
+        self.save()
 
     @classmethod
     def update_all(cls):
@@ -47,6 +55,25 @@ class Entity(db.DynamicDocument):
         for entity in all_entities:
             entity.reset_frequencies()
             entity.reset_channels()
+
+    @classmethod
+    def add(cls, data_set: str):
+        # Сразу складываем в строчку  приводя  к унифицированой последовательности вида 1110111
+        sequence = ''.join(re.findall(re_sequence, data_set))
+        channel = int(re.findall(re_channel, data_set)[0])  # Узнаем номер канала,что учитывать частотность по нему.
+        # Возможно такая последовательность уже есть у нас в БД, поэтому стоит проверить
+        entities = Entity.objects.filter(sequence=sequence)
+        if entities:
+            # Обновим эту запись
+            entity = entities[0]
+        else:
+            # Нужна новая запись
+            entity = Entity.objects.create(sequence=sequence)
+
+        frequencies = entity.get_frequencies()
+        frequencies[channel] += 1  # Увеличим счетчик "попаданий" на канал
+        entity.set_frequencies(frequencies)
+        entity.save()
 
     @classmethod
     def get_channel_by_key(cls, key: str) -> int:
--- entities/views.py
+++ entities/views.py
@@ -1,5 +1,7 @@
+import json
+
 import flask
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template
 from flask.views import MethodView
 
 entities = Blueprint('entities', __name__, template_folder='templates')
@@ -11,7 +13,25 @@ class EntityView(MethodView):
 
 
 class EntityAdd(MethodView):
-    def post(self):
+    def post(self, *args, **kwargs):
+        from .models import Entity as Records
+        data = json.loads(flask.request.data.decode())
+        learn_data = data.get('learn_data')  # Всё содержание обучающего файла
+        learn_data = learn_data.replace('\r\n', '\n')  # Унификация для файлов созданых в Win
+        data_sets = learn_data.split('\n')
+        pointer = 0  # Указатель на позицию чтения data_sets
+        # Идем по набору данных пока не встретим информацию о количестве входных значений
+        for data_set in data_sets:
+            pointer += 1
+            if -1 != data_set.find('@data'):
+                # Следующей строчкой пойдут обучающие данные.Пора прерваться
+                break
+        raw_data_sets = data_sets[pointer:]
+        raw_data_sets = filter(lambda x: x != '', raw_data_sets)  # Игнорируем пустые строки
+        for data_set in raw_data_sets:
+            Records.add(data_set)  # Сложим все записи в хранилище
+        # После чего стоит пересчмитать данные и одновить укзатели на каналы которые мы считаем приоритетными
+        Records.update_all()
         response = {'status': 'Database was updated'}
         return flask.jsonify(**response)
 
@@ -20,7 +40,7 @@ class EntityTest(MethodView):
     def post(self):
         response = {'status': 'Comprehension complete',
                     'pairs': [{'key': '0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0', 'value': 0},
-                              {'key': '1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0', 'value':1}]}
+                              {'key': '1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0', 'value': 1}]}
         return flask.jsonify(**response)
 
 
